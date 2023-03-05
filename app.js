b3dd737732e49b0944f1f85bc2b8dd833ad3548e
M AKIF <makifhussainmn@gmail.com>
2023-03-05 23:35:37 +0500

    6 march 2023

---
+++ app.js
@@ -0,0 +1,26 @@
+alert ("Akif");
+console.log (22);
+Document.write ( akif )
+
+;
+
+Let a = prompt;
+if a=2 && a=2{
+
+
+   alert  ("correct" )
+}
+else {
+    alert("incorrect")
+}
+
+
+
+// Let a = prompt;
+// if a=2 && a=2{
+
+
+//    alert  ("correct" )
+// }
+// else {
+//     alert("incorrect")
