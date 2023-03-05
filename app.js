ac81d828333956014049ddc2d8ef9bbfc7a10bc5
M AKIF <makifhussainmn@gmail.com>
2023-03-05 23:41:38 +0500

    javascript and html practice

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-alert ("Akif");
-console.log (22);
-Document.write ( akif )
+// alert ("Akif");
+// console.log (22);
+// Document.write ( akif )
 
-;
-
-Let a = prompt;
-if a=2 && a=2{
-
-
-   alert  ("correct" )
-}
-else {
-    alert("incorrect")
-}
-
-
+// ;
 
 // Let a = prompt;
 // if a=2 && a=2{
@@ -24,3 +12,17 @@ else {
 // }
 // else {
 //     alert("incorrect")
+// }
+
+
+
+Let a = prompt ;
+if else ( a=2 && a=5){
+
+
+document.write ("correct" )
+};
+else {
+    document.write("incorrect")
+}
+
